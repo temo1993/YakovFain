622da9c92c505ba86660c957f025dec3d512af01
temo1993 <temo.kiknavelidze@gmail.com>
2016-02-05 13:01:32 +0400

    ...

---
+++ src/try_It/lesson8/swingUI/Operations.java
@@ -0,0 +1,16 @@
+
+/**
+
+ * Using eum to simplify the addition 
+ * of a new operation if need be
+ * 
+ */
+package try_It.lesson8.swingUI;
+
+public enum Operations {
+	ADDITION,
+	SUBTRACTION,
+	DIVISION,
+	MULTIPLYING,
+	ILLEGAL_OPERATION
+}
