df540f8868f851b7f4dbb5ba6d3cbd1d68c2439d
Temo <temo.kiknavelidze@gmail.com>
2016-02-20 04:31:29 +0400

    ...

---
+++ src/lesson20/streamAPI/sorting/comparator/Beer.java
@@ -0,0 +1,17 @@
+package lesson20.streamAPI.sorting.comparator;
+
+public class Beer {
+    public String name;
+    public String country;
+    public float price;
+
+    Beer(String name, String country,float price){
+        this.name=name;
+        this.country=country;
+        this.price=price;
+    }
+
+    public String toString(){
+        return "Name: " + name + ", price: " + price;
+    }
+}
